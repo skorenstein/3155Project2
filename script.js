0f4ea7a01b91c1ca812f2a33f751e6e61666ab96
Luke_Malonis <malonisluke@gmail.com>
2025-11-06 12:21:54 -0500

    chore: moodboard + css changes

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 /**
  * Toggles the display of an individual team member's bio
  * @param {string} bioId - The ID of the bio section to show or hide
+ * @param {string} moodboardId - The ID of the moodboard container (optional)
  */
-function toggleBio(bioId) {
+function toggleBio(bioId, moodboardId = null) {
     const bio = document.getElementById(bioId);
     // Toggle between showing and hiding the bio section
     if (bio.style.display === "none" || bio.style.display === "") {
         bio.style.display = "block";
+
+        // If moodboard ID is provided, show the moodboard as well
+        if (moodboardId) {
+            const moodboard = document.getElementById(moodboardId);
+            moodboard.style.display = "block";
+        }
     } else {
         bio.style.display = "none";
+
+        // If moodboard ID is provided, hide the moodboard as well
+        if (moodboardId) {
+            const moodboard = document.getElementById(moodboardId);
+            moodboard.style.display = "none";
+        }
     }
 }
 
